e5bc9729520aaaed871a11d1642d2d27a1db2d00
Straw17 <strawmcstick@gmail.com>
2019-11-10 15:42:17 -0500

    first commit

---
+++ src/main/java/frc/robot/EastCoastDrive.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class EastCoastDrive {
+    CANSparkMax lFront, rFront, lMaster, rMaster, lRear, rRear;
+    DifferentialDrive drive;
+    
+}
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,5 @@
+package frc.robot;
+
+public class RobotMap {
+    public static final int LFRONT;
+}
